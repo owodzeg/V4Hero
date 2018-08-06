f652557122bca22eeef8970a3c21f248c5a89ce2
owocek <owocektv@gmail.com>
2018-08-07 00:20:34 +0200

    Cleanup pt. 3
    EVERYTHING. COMMENTED. AND CLEANED. (for now)

--- Engine/Rhythm.cpp
+++ Engine/Rhythm.cpp
@@ -52,6 +52,7 @@ Rhythm::Rhythm()
 
 void Rhythm::LoadTheme(string theme)
 {
+    ///Load the BGM
     b_theme[0].loadFromFile("resources/sfx/bgm/"+theme+"/bgm_start.ogg");
     b_theme[1].loadFromFile("resources/sfx/bgm/"+theme+"/bgm_1.ogg");
     b_theme[2].loadFromFile("resources/sfx/bgm/"+theme+"/bgm_2.ogg");
@@ -81,8 +82,10 @@ void Rhythm::LoadTheme(string theme)
     b_theme[26].loadFromFile("resources/sfx/bgm/"+theme+"/bgm_fever15.ogg");
     b_theme[27].loadFromFile("resources/sfx/bgm/"+theme+"/bgm_fever16.ogg");
 
+    ///Fever die sound
     b_theme[28].loadFromFile("resources/sfx/bgm/fever_fail.ogg");
 
+    ///All chants
     b_chant["patapata_1"].loadFromFile("resources/sfx/bgm/"+theme+"/s_patapata1.ogg");
     b_chant["patapata_2"].loadFromFile("resources/sfx/bgm/"+theme+"/s_patapata2.ogg");
     b_chant["patapata_3"].loadFromFile("resources/sfx/bgm/"+theme+"/s_patapata3.ogg");
@@ -108,53 +111,57 @@ void Rhythm::LoadTheme(string theme)
     b_chant["dondon_2"].loadFromFile("resources/sfx/bgm/"+theme+"/s_dondon2.ogg");
     b_chant["dondon_3"].loadFromFile("resources/sfx/bgm/"+theme+"/s_dondon3.ogg");
 
-
+    ///Play the BGM beginning after loading
     s_theme[0].setBuffer(b_theme[0]);
     s_theme[0].play();
 
+    ///Restart the Rhythm clocks
     rhythmClock.restart();
     masterTimer = 500;
 }
 
 void Rhythm::Draw(sf::RenderWindow& window)
 {
-    bool broke = false;
+    bool breakCombo = false; ///Initialize a temporary value used for breaking the combo
 
-    if(rhythmClock.getElapsedTime().asSeconds() >= 4)
+    if(rhythmClock.getElapsedTime().asSeconds() >= 4) ///2 Commands elapsed (1 - Patapons singing, 2 - User input)
     {
-        if(combo >= 2)
+        if(combo >= 2) ///If combo is bigger than 2 (The rhythm started)
         {
-            if(command.size() == 4)
+            if(commandInput.size() == 4) ///The size of user input must be exactly 4 hits (to be changed later for DON DODON DODON support)
             {
-                string fullcom = command[0]+command[1]+command[2]+command[3];
+                string fullcom = commandInput[0]+commandInput[1]+commandInput[2]+commandInput[3]; ///Create a full command using 4 individual hits
 
-                if(std::find(av_commands.begin(), av_commands.end(), fullcom) != av_commands.end())
+                if(std::find(av_commands.begin(), av_commands.end(), fullcom) != av_commands.end()) ///Check if the command exists in available commands
                 {
-                    command.clear();
+                    ///Clear user input
+                    commandInput.clear();
 
+                    ///Push the amount of perfect hits to the table and reset them
                     perfects.push_back(perfect);
                     perfect = 0;
 
-                    float total_perfects = 0;
-
-                    if(perfects.size() > acc_count)
+                    ///Remove unnecessary perfects
+                    while(perfects.size() > acc_count)
                     {
                         perfects.erase(perfects.begin());
                     }
 
-                    for(int i=0; i<=acc_count; i++)
+                    ///Calculate total perfect value
+                    float total_perfects = 0;
+
+                    for(int i=0; i<acc_count; i++)
                     {
-                        if(perfects.size() > i)
-                        {
-                            total_perfects += perfects[i];
-                        }
+                        total_perfects += perfects[i];
                     }
 
+                    ///Calculate beat accuracy
                     accuracy = total_perfects / (4 * perfects.size());
-                    cout << "Total accuracy: " << accuracy*100 << "%" << endl;
 
+                    ///Increment combo
                     combo++;
 
+                    ///Check for prefever accuracy to determine combo skip
                     if((combo <= 10) && (combo >= 6))
                     {
                         if(accuracy >= 0.875 - (0.025 * (combo - 7)))
@@ -170,7 +177,7 @@ void Rhythm::Draw(sf::RenderWindow& window)
                         }
                     }
 
-                    cout << "Command is being inputted - play the chant here." << endl;
+                    ///Calculate which chant should be used
                     int chant_id = 0;
 
                     if(combo < 6)
@@ -193,6 +200,7 @@ void Rhythm::Draw(sf::RenderWindow& window)
                         }
                     }
 
+                    ///Determine which song should be played
                     int song_ID = -1;
 
                     for(int i=0; i<av_commands.size(); i++)
@@ -203,8 +211,7 @@ void Rhythm::Draw(sf::RenderWindow& window)
                         }
                     }
 
-                    cout << "combo is equal to: " << combo << endl;
-
+                    ///Prevent chants from playing at FEVER combo
                     if(combo != 11)
                     {
                         string chant_name = av_songs[song_ID]+"_"+to_string(chant_id);
@@ -215,73 +222,89 @@ void Rhythm::Draw(sf::RenderWindow& window)
                 }
                 else
                 {
-                    broke = true;
+                    ///Break the combo if command is not found
+                    breakCombo = true;
                 }
             }
             else
             {
-                broke = true;
+                ///Break the combo if user input was less than 4 drums
+                breakCombo = true;
             }
         }
 
+        ///Set the combo to 1 if Rhythm system just started
         if(combo == 0)
         combo = 1;
 
+        ///Set the combo back to fever beginning, when it reaches the end of BGM
         if(combo >= 28)
         combo = 12;
 
+        ///Set the master timer to proper value
         masterTimer = 450;
         masterTimerMode = 0;
 
+        ///Play the BGM
         s_theme[combo%2].setBuffer(b_theme[combo]);
 
         s_theme[combo%2].stop();
         s_theme[combo%2].play();
 
+        ///Reset command and beat values
         commandValue = 1;
         beatValue = 1;
 
+        ///Reset Rhythm Clock
         rhythmClock.restart();
     }
 
+    ///When Master timer has reached max value (PERFECT 500 ms, one beat)
     if(masterTimer >= 500)
     {
-        if((commandValue == 2) && (combo >= 2) && (command.size() < beatValue-1))
+        ///Check if command value is set at 2 (User input), if combo is equal or above 2, check if user input is correct
+        if((commandValue == 2) && (combo >= 2) && (commandInput.size() < beatValue-1))
         {
-            broke = true;
+            breakCombo = true;
         }
 
+        ///Increment the beat value
         beatValue++;
 
+        ///If 4 beats has passed, increment the command value
         if(beatValue > 4)
         {
             beatValue = 1;
             commandValue++;
         }
 
+        ///Reset master timer and change the mode
         masterTimerMode = 1;
         masterTimer = 500;
     }
     else if(masterTimer <= 0)
     {
+        ///Reset master timer and change the mode
         masterTimerMode = 0;
         masterTimer = 0;
 
+        ///Disable drum block
         drumAlreadyHit = false;
     }
 
+    ///Visuals
     if(masterTimerMode == 1)
     r_rhythm.setFillColor(sf::Color(255,255,255,masterTimer/float(2)));
     window.draw(r_rhythm);
 
+    ///Master Timer speed (Higher value = Faster Rhythm system)
     if(masterTimerMode == 1)
     masterTimer -= float(2000) / fps;
 
     if(masterTimerMode == 0)
     masterTimer += float(2000) / fps;
 
-    //cout << combo << endl;
-
+    ///Set initial values for Drum quality check
     int drum_quality = 2;
     bool add_to_commandtable = false;
 
@@ -295,7 +318,7 @@ void Rhythm::Draw(sf::RenderWindow& window)
         }
         else if((masterTimer >= low_range) && (masterTimer < high_range)) ///GOOD hit
         {
-            ///Add drum to command table
+            ///Add drum to commandInput table
             add_to_commandtable = true;
 
             ///Apply GOOD drum sound effect
@@ -303,7 +326,7 @@ void Rhythm::Draw(sf::RenderWindow& window)
         }
         else if(masterTimer >= high_range)
         {
-            ///Add drum to command table
+            ///Add drum to commandInput table
             add_to_commandtable = true;
 
             ///Apply BEST drum sound effect
@@ -311,69 +334,69 @@ void Rhythm::Draw(sf::RenderWindow& window)
         }
     }
 
+    ///Keybind for PATA drum
     if(keyMap[config.GetInt("keybindPata")] || keyMap[config.GetInt("secondaryKeybindPata")])
     {
+        ///Add PATA drum sound effect to the buffer
         drum_nc.setBuffer(b_pata[drum_quality]);
         drum_c.setBuffer(b_chpata[drum_quality]);
 
+        ///Add PATA drum to user input table
         if(add_to_commandtable)
-        command.push_back("PATA");
+        commandInput.push_back("PATA");
     }
 
+    ///Keybind for PON drum
     if(keyMap[config.GetInt("keybindPon")] || keyMap[config.GetInt("secondaryKeybindPon")])
     {
+        ///Add PON drum sound effect to the buffer
         drum_nc.setBuffer(b_pon[drum_quality]);
         drum_c.setBuffer(b_chpon[drum_quality]);
 
+        ///Add PON drum to user input table
         if(add_to_commandtable)
-        command.push_back("PON");
+        commandInput.push_back("PON");
     }
 
+    ///Keybind for CHAKA drum
     if(keyMap[config.GetInt("keybindChaka")] || keyMap[config.GetInt("secondaryKeybindChaka")])
     {
+        ///Add CHAKA drum sound effect to the buffer
         drum_nc.setBuffer(b_chaka[drum_quality]);
         drum_c.setBuffer(b_chchaka[drum_quality]);
 
+        ///Add CHAKA drum to user input table
         if(add_to_commandtable)
-        command.push_back("CHAKA");
+        commandInput.push_back("CHAKA");
     }
 
+    ///Keybind for DON drum
     if(keyMap[config.GetInt("keybindDon")] || keyMap[config.GetInt("secondaryKeybindDon")])
     {
+        ///Add DON drum sound effect to the buffer
         drum_nc.setBuffer(b_don[drum_quality]);
         drum_c.setBuffer(b_chdon[drum_quality]);
 
+        ///Add DON drum to user input table
         if(add_to_commandtable)
-        command.push_back("DON");
+        commandInput.push_back("DON");
     }
 
     ///IF statement that applies to all drum keybinds (to not repeat the same code over and over)
     if(keyMap[config.GetInt("keybindPata")] || keyMap[config.GetInt("secondaryKeybindPata")] || keyMap[config.GetInt("keybindPon")] || keyMap[config.GetInt("secondaryKeybindPon")] || keyMap[config.GetInt("keybindDon")] || keyMap[config.GetInt("secondaryKeybindDon")] || keyMap[config.GetInt("keybindChaka")] || keyMap[config.GetInt("secondaryKeybindChaka")])
     {
-        ///If combo is less than 1,
-        if(combo <= 1)
-        {
-            if(drumAlreadyHit == true)
-            command.clear();
-        }
-        else
-        {
-            if(drumAlreadyHit == true)
-            broke = true;
-        }
-
-        if(masterTimer < low_range)
+        ///If drum was already hit and you hit once again, or you hit BAD, reset user input and break combo
+        if((drumAlreadyHit == true) || (masterTimer < low_range))
         {
             command_perfects.clear();
             perfects.clear();
-            command.clear();
+            commandInput.clear();
 
             if(combo >= 2)
-            {
-                broke = true;
-            }
+            breakCombo = true;
         }
 
+        ///If drum was hit above the minimum range, determine it's quality and mark as being already hit
         if(masterTimer >= low_range)
         {
             bool perfect_command = false;
@@ -387,42 +410,46 @@ void Rhythm::Draw(sf::RenderWindow& window)
             drumAlreadyHit = true;
         }
 
+        ///Check config if drum sound effect should be played
         if(config.GetInt("enableDrums"))
         {
+            ///And play it
             s_drums.push_back(drum_nc);
             s_drums[s_drums.size()-1].play();
         }
 
+        ///Check config if drum chant sound effect should be played
         if(config.GetInt("enableDrumChants"))
         {
+            ///And play it
             s_drums.push_back(drum_c);
             s_drums[s_drums.size()-1].play();
         }
 
-        if(command.size() >= 5)
-        command.erase(command.begin());
+        ///Remove drums from user input if user has hit more than 4 drums
+        if(commandInput.size() > 4)
+        commandInput.erase(commandInput.begin());
 
+        ///Restart the Timeout and Before Fever clocks every time a drum is being hit
         commandTimeout.restart();
         beforeFeverClock.restart();
 
-        if(commandValue == 1)
+        ///If you start drumming when commandValue is at 1 (Patapon singing), break the combo
+        if((commandValue == 1) && (combo >= 2))
         {
-            if(combo >= 2)
-            {
-                broke = true;
-            }
+            breakCombo = true;
         }
 
-        while(command_perfects.size() > 4)
-        command_perfects.erase(command_perfects.begin());
-
-        perfect = 0;
-
-        for(int i=0; i<=command_perfects.size(); i++)
+        ///Calculate how many perfect beats were in the command
+        if(command_perfects.size() >= 4)
         {
-            perfect += command_perfects[i];
+            while(command_perfects.size() > 4)
+            command_perfects.erase(command_perfects.begin());
+
+            perfect = command_perfects[0]+command_perfects[1]+command_perfects[2]+command_perfects[3];
         }
 
+        ///Reset all keybindings
         keyMap[config.GetInt("keybindPata")] = false;
         keyMap[config.GetInt("secondaryKeybindPata")] = false;
         keyMap[config.GetInt("keybindPon")] = false;
@@ -433,42 +460,51 @@ void Rhythm::Draw(sf::RenderWindow& window)
         keyMap[config.GetInt("secondaryKeybindDon")] = false;
     }
 
+    ///Timeout for inputting drums, clear all user input when user didn't took action for longer than a second
     if(commandTimeout.getElapsedTime().asSeconds() >= 1)
     {
-        command.clear();
+        commandInput.clear();
     }
 
+    ///Before Fever Clock function, made specifically to be able to start drumming from any point in music (when idle)
     if(beforeFeverClock.getElapsedTime().asMilliseconds() >= 505)
     {
-        if(command.size() == 4)
+        if(commandInput.size() == 4) ///Check if there are exactly 4 drums in user input
         {
-            if(combo <= 1)
+            if(combo <= 1) ///If combo is less or equal to 1 (because it's the idle loop, aka the start)
             {
-                string fullcom = command[0]+command[1]+command[2]+command[3];
+                string fullcom = commandInput[0]+commandInput[1]+commandInput[2]+commandInput[3]; ///Compose one big command string
 
-                if(std::find(av_commands.begin(), av_commands.end(), fullcom) != av_commands.end())
+                if(std::find(av_commands.begin(), av_commands.end(), fullcom) != av_commands.end()) ///Check if the command exists
                 {
+                    ///Reset the Rhythm Clock
                     rhythmClock.restart();
 
+                    ///Reset the initial values
                     beatValue = 1;
                     commandValue = 1;
                     masterTimer = 450;
                     masterTimerMode = 0;
+
+                    ///Increment the combo to BGM starting point
                     combo = 2;
 
+                    ///Push the amount of perfect drums to the Perfects table
                     perfects.push_back(perfect);
-                    cout << "Perfect drums: " << perfect << endl;
                     perfect = 0;
 
-                    command.clear();
+                    ///Clear user input
+                    commandInput.clear();
+
+                    ///Reset BGM
                     s_theme[0].stop();
                     s_theme[1].stop();
 
+                    ///Start playing BGM from the starting point
                     s_theme[0].setBuffer(b_theme[combo]);
                     s_theme[0].play();
 
-
-
+                    ///Determine the song ID
                     int song_ID = -1;
 
                     for(int i=0; i<av_commands.size(); i++)
@@ -479,6 +515,7 @@ void Rhythm::Draw(sf::RenderWindow& window)
                         }
                     }
 
+                    ///Determine the chant ID
                     string chant_name = av_songs[song_ID]+"_1";
                     s_chant.stop();
                     s_chant.setBuffer(b_chant[chant_name]);
@@ -488,9 +525,10 @@ void Rhythm::Draw(sf::RenderWindow& window)
         }
     }
 
-    if(broke)
+    ///Break Combo function (NOTE: can be ported to a function in class later)
+    if(breakCombo)
     {
-        if(combo >= 11)
+        if(combo >= 11) ///Play dying fever sound when BGM is in fever state
         {
             ///Dying fever sound
             s_fever_fail.setBuffer(b_theme[28]);
@@ -498,21 +536,30 @@ void Rhythm::Draw(sf::RenderWindow& window)
 
         }
 
+        ///Reset Rhythm Clock
         rhythmClock.restart();
 
+        ///Reset initial values
         beatValue = 1;
         commandValue = 1;
         masterTimer = 450;
         masterTimerMode = 0;
+
+        ///Reset combo to idle BGM point
         combo = 1;
 
+        ///Reset Perfects table
         perfect = 0;
         perfects.clear();
 
-        command.clear();
+        ///Reset command input
+        commandInput.clear();
+
+        ///Reset BGM
         s_theme[0].stop();
         s_theme[1].stop();
 
+        ///Play BGM from the combo of idle point
         s_theme[0].setBuffer(b_theme[combo]);
         s_theme[1].play();
     }
--- Engine/Rhythm.h
+++ Engine/Rhythm.h
@@ -11,21 +11,21 @@ class Rhythm
 {
     private:
     /// Theme and chant buffers ///
-    std::map<int,sf::SoundBuffer> b_theme;
-    std::map<std::string,sf::SoundBuffer> b_chant;
+    std::map<int,sf::SoundBuffer> b_theme; ///Sound buffer for main theme
+    std::map<std::string,sf::SoundBuffer> b_chant; ///Sound buffer for Patapon chants
 
 
 
     /// Drums ///
-    sf::SoundBuffer b_pata[3],b_pon[3],b_chaka[3],b_don[3];
-    sf::SoundBuffer b_chpata[3],b_chpon[3],b_chchaka[3],b_chdon[3];
-    std::vector<sf::Sound> s_drums;
+    sf::SoundBuffer b_pata[3],b_pon[3],b_chaka[3],b_don[3]; ///Drums without chants
+    sf::SoundBuffer b_chpata[3],b_chpon[3],b_chchaka[3],b_chdon[3]; ///Drums with chants
+    std::vector<sf::Sound> s_drums; ///Table for storing all the drums being hit
 
 
 
     /// Low and high range for BAD, GOOD and BEST hits (in milliseconds) ///
-    int low_range = 300;
-    int high_range = 425;
+    int low_range = 300; ///Anything below that range will be treated as BAD hit
+    int high_range = 425; ///Anything between this and low range will be treated as GOOD hit. Higher will be treated as BEST hit.
 
 
 
@@ -39,33 +39,46 @@ class Rhythm
 
 
     /// Initialize clocks ///
-    sf::Clock rhythmClock;
-    sf::Clock commandTimeout;
-    sf::Clock beforeFeverClock;
+    sf::Clock rhythmClock; ///Main clock for Rhythm purposes
+    sf::Clock commandTimeout; ///Timeout clock to break the combo after not inputting any drum for a while
+    sf::Clock beforeFeverClock; ///Clock used while inputting first commands, later it is useless
 
 
 
     /// Initialize Rhythm System values ///
-    int combo = 1;
-    float masterTimer = 500;
-    int masterTimerMode = 0;
-    int beatValue = 1;
-    int commandValue = 1;
-    bool drumAlreadyHit = false;
+    int combo = 1; ///Rhythm combo, main navigator through BGM
+    float masterTimer = 500; ///Master Timer, determines the quality of hit drums
+    int masterTimerMode = 0; ///Mode of Master Timer, for synchronizing the time ranges properly
+    int beatValue = 1; ///Value for beats (drums) in a command (1, 2, 3, 4)
+    int commandValue = 1; ///Value for current command (1 - Patapons sing, 2 - Player input)
+    bool drumAlreadyHit = false; ///Made to check if drum has already been hit in 1 beat (to prevent from hitting multiple drums at a time)
 
-    std::vector<std::string> command;
-    std::vector<std::string> av_commands = {"PATAPATAPATAPON","PONPONPATAPON","CHAKACHAKAPATAPON","PONPONCHAKACHAKA","PATAPONDONCHAKA","PONPATAPONPATA","DONDONCHAKACHAKA","CHAKAPATACHAKAPATA"};
-    std::vector<std::string> av_songs = {"patapata","ponpon","chakachaka","ponchaka","donchaka","ponpata","dondon","patapata"};
-    std::string song;
+    std::vector<std::string> commandInput; ///Drum input
+    std::vector<std::string> av_commands = {"PATAPATAPATAPON",
+                                            "PONPONPATAPON",
+                                            "CHAKACHAKAPATAPON",
+                                            "PONPONCHAKACHAKA",
+                                            "PATAPONDONCHAKA",
+                                            "PONPATAPONPATA",
+                                            "DONDONCHAKACHAKA",
+                                            "CHAKAPATACHAKAPATA"}; ///Available commands
+    std::vector<std::string> av_songs = {"patapata",
+                                        "ponpon",
+                                        "chakachaka",
+                                        "ponchaka",
+                                        "donchaka",
+                                        "ponpata",
+                                        "dondon",
+                                        "patapata"}; ///Available songs
 
 
 
     /// Perfction calculator ///
-    int perfect = 0;
-    std::vector<int> command_perfects;
-    std::vector<int> perfects;
-    float accuracy = 0;
-    int acc_count = 3;
+    int perfect = 0; ///value used for calculating perfect drums in a command
+    std::vector<int> command_perfects; ///vector used for storing perfection of each drum
+    std::vector<int> perfects = {4,4,4,4,4}; ///vector of perfections of inputted commands (with default data)
+    float accuracy = 0; ///value for calculating the accuracy
+    int acc_count = 3; ///value for determining on how far back should the accuracy calculation system go in commands
 
 
 
