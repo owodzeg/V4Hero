ed8fb3f4fc78f3da9e81f8e15e6309395c55b21b
owocek <owocektv@gmail.com>
2019-12-27 00:05:31 +0100

    1.0.0 release fixes

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <windows.h>
 
 #include "Engine/V4Core.h"
 
@@ -7,12 +8,17 @@
 #include <fcntl.h>
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
     cout << "V4" << endl;
     //_setmode(_fileno(stdout), _O_U16TEXT);
     //wchar_t * unicode_text = L"aäbcdefghijklmnoöpqrsßtuüvwxyz";
     //wprintf(L"%s", unicode_text);
+
+    ///Fix for buggy CreateProcess not copying the working directory properly
+    string wd = argv[0];
+    wd = wd.substr(0,wd.find_last_of("\\"));
+    SetCurrentDirectoryA(wd.c_str());
 
     V4Core V4;
     V4.Init();
