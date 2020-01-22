9ed4ea287899ad24f7ade246bb417384cbe1e181
owocek <owocektv@gmail.com>
2020-01-22 23:06:56 +0100

    change default theme for 2nd mission

--- Engine/Config.h
+++ Engine/Config.h
@@ -58,7 +58,7 @@ class Config
     std::to_string(sf::Keyboard::Escape),
     "Hey Whazzat Rock Theme",
     "nanjarohills",
-    "Freakout Rock Theme",
+    "Pokkurimakka Rock Theme",
     "evilmass",
     "0",
     "0",
