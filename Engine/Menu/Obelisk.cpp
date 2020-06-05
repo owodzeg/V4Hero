4062d882ac0529f3d16587d072b5760a417dbbcb
owocek <owocektv@gmail.com>
2020-06-05 04:28:29 +0200

    minor improvements 1/2

--- Engine/Menu/Obelisk.cpp
+++ Engine/Menu/Obelisk.cpp
@@ -6,105 +6,347 @@
 #include <sstream>
 ObeliskMenu::ObeliskMenu()
 {
-    f_font.loadFromFile("resources/fonts/p4kakupop-pro.ttf");
-    //f_font.loadFromFile("resources/fonts/arial.ttf");
-    t_titlemenu.setFont(f_font);
-    t_titlemenu.setCharacterSize(38);
-    t_titlemenu.setFillColor(sf::Color::Black);
-
-    t_itemtitle.setFont(f_font);
-    t_itemtitle.setCharacterSize(28);
-    t_itemtitle.setFillColor(sf::Color::Black);
-
-
-
-
-
     isActive=false;
 }
 void ObeliskMenu::Initialise(Config *thisConfigs,std::map<int,bool> *keymap,V4Core *parent, PatapolisMenu *curParentMenu){
     Scene::Initialise(thisConfigs,keymap,parent);
     parentMenu = curParentMenu;
 
-    mm_inventory_background.setSize(sf::Vector2f(thisConfig->GetInt("resX"),thisConfig->GetInt("resY")));
-    mm_inventory_background.setFillColor(sf::Color(0,0,0,190));
-    obeliskButtonList.Initialise(&f_font,*thisConfig,keymap,&(v4core->currentController),this);
-    //buttonList.Initialise(&m_font,*thisConfig,keymap,&(v4core->currentController),this);
+    font.loadFromFile("resources/fonts/p4kakupop-pro.ttf");
 
+    int quality = thisConfig->GetInt("textureQuality");
+
+    float ratioX, ratioY;
+
+    switch(quality)
+    {
+        case 0: ///low
+        {
+            ratioX = thisConfigs->GetInt("resX") / float(640);
+            ratioY = thisConfigs->GetInt("resY") / float(360);
+            break;
+        }
+
+        case 1: ///med
+        {
+            ratioX = thisConfigs->GetInt("resX") / float(1280);
+            ratioY = thisConfigs->GetInt("resY") / float(720);
+            break;
+        }
+
+        case 2: ///high
+        {
+            ratioX = thisConfigs->GetInt("resX") / float(1920);
+            ratioY = thisConfigs->GetInt("resY") / float(1080);
+            break;
+        }
+
+        case 3: ///ultra
+        {
+            ratioX = thisConfigs->GetInt("resX") / float(3840);
+            ratioY = thisConfigs->GetInt("resY") / float(2160);
+            break;
+        }
+    }
+
+    ///worldmap contents
+    for(int i=1; i<=3; i++)
+    {
+        PSprite bg;
+        bg.loadFromFile("resources/graphics/ui/worldmap/locationbg_"+to_string(i)+".png", quality, 1);
+        PSprite loc;
+        loc.loadFromFile("resources/graphics/ui/worldmap/location_"+to_string(i)+".png", quality, 1);
+
+        location_bgs.push_back(bg);
+        location_icons.push_back(loc);
+    }
+
+    PSprite i_hunt;
+    i_hunt.loadFromFile("resources/graphics/ui/worldmap/hunting_icon.png", quality, 1);
+    PSprite i_fortress;
+    i_fortress.loadFromFile("resources/graphics/ui/worldmap/fortress_icon.png", quality, 1);
+
+    mission_icons.push_back(i_hunt);
+    mission_icons.push_back(i_fortress);
+
+    dullpon.loadFromFile("resources/graphics/ui/worldmap/dullpon.png", quality, 1);
+
+    PSprite fld;
+    fld.loadFromFile("resources/graphics/ui/worldmap/location_field.png", quality, 1);
+
+    for(int i=1; i<=20; i++)
+    {
+        worldmap_fields.push_back(fld);
+
+        if(i <= location_icons.size())
+        {
+            worldmap_icons.push_back(location_icons[i-1]);
+        }
+        else
+        {
+            worldmap_icons.push_back(dullpon);
+        }
+    }
+
+    ///boxes
+    mainbox.loadFromFile("resources/graphics/ui/worldmap/main_box.png", quality, 1);
+    mainbox.setOrigin(mainbox.getLocalBounds().width/2, mainbox.getLocalBounds().height/2);
+    mainbox.setPosition(640, 320);
+    descbox.loadFromFile("resources/graphics/ui/worldmap/description_box.png", quality, 1);
+    descbox.setOrigin(descbox.getLocalBounds().width/2, descbox.getLocalBounds().height/2);
+    descbox.setPosition(820, 542);
+    iconbox.loadFromFile("resources/graphics/ui/worldmap/icon_box.png", quality, 1);
+    missionbox.loadFromFile("resources/graphics/ui/worldmap/mission_box.png", quality, 1);
+    missionbox.setOrigin(missionbox.getLocalBounds().width/2, missionbox.getLocalBounds().height/2);
+    missionbox.setPosition(290, 542);
+    missionselect.loadFromFile("resources/graphics/ui/worldmap/mission_select.png", quality, 1);
+
+    worldmap_title.createText(font, 34, sf::Color::Black, "Patapon World Map", quality, 1);
+    location_title.createText(font, 27, sf::Color::Black, "~ Mt. Gonrok ~", quality, 1);
+    location_desc.createText(font, 17, sf::Color::Black, "(no translation needed)", quality, 1);
+    select_quest.createText(font, 18, sf::Color::Black, "Select a quest", quality, 1);
+    mission_title.createText(font, 18, sf::Color::Black, "Hunting Kacheek", quality, 1);
+    mission_desc.createText(font, 18, sf::Color::Black, "(no translation needed)", quality, 1);
+
+    location_bg_a = location_bgs[cur_location];
+    location_bg_b = location_bgs[cur_location];
+
+    float resRatioX = thisConfigs->GetInt("resX") / float(1280);
+    float resRatioY = thisConfigs->GetInt("resY") / float(720);
+
+    unavailable.loadFromFile("resources/graphics/ui/worldmap/unavailable.png", quality, 1);
+    location_highlight.loadFromFile("resources/graphics/ui/worldmap/location_highlight.png", quality, 1);
+    location_highlight.setOrigin(location_highlight.getLocalBounds().width/2, location_highlight.getLocalBounds().height/2);
+
+    render_map.create(1012*resRatioX, 162*resRatioY);
+    cout << "[WorldMap] Creating render_map " << 1012*resRatioX << "x" << 162*resRatioY << endl;
 }
-void ObeliskMenu::EventFired(sf::Event event){
-    if(isActive){
+
+void ObeliskMenu::EventFired(sf::Event event)
+{
+    if(isActive)
+    {
         if(event.type == sf::Event::KeyPressed)
         {
-            obeliskButtonList.KeyPressedEvent(event);
-            // do something here;
-            if (event.key.code == sf::Keyboard::Num1){
-
-
-            } else if (event.key.code == sf::Keyboard::Num2){
-
-
-
-            } else if (event.key.code == sf::Keyboard::Num3){
-
-
-
-            } else if (event.key.code == sf::Keyboard::Num4){
-
-
-
-            } else if (event.key.code == sf::Keyboard::Num5){
-
-
-            } else if (event.key.code == thisConfig->GetInt("keybindPon") || event.key.code == thisConfig->GetInt("secondaryKeybindPon")){
-
-
-            } else if (event.key.code == thisConfig->GetInt("keybindPata") || event.key.code == thisConfig->GetInt("secondaryKeybindPata")){
-
-            } else if (event.key.code == thisConfig->GetInt("keybindChaka") || event.key.code == thisConfig->GetInt("secondaryKeybindChaka")){
-
-
-            } else if (event.key.code == thisConfig->GetInt("keybindDon") || event.key.code == thisConfig->GetInt("secondaryKeybindDon")){
-
-
-            } else if (event.key.code == thisConfig->GetInt("keybindBack")){
+            if(event.key.code == thisConfig->GetInt("keybindBack"))
+            {
                 this->Hide();
                 this->isActive = false;
             }
+            else if(event.key.code == sf::Keyboard::M)
+            {
+                if(!displayMissions)
+                displayMissions = true;
+                else
+                displayMissions = false;
+            }
+            else if(event.key.code == sf::Keyboard::B)
+            {
+                ///swap bg
+                cur_location += 1;
 
-        } else if (event.type == sf::Event::MouseButtonReleased){
-            // We use mouse released so a user can change their mind by keeping the mouse held and moving away.
-            obeliskButtonList.MouseReleasedEvent(event);
+                if(cur_location >= location_bgs.size())
+                cur_location = 0;
+
+                location_bg_a = location_bg_b;
+                location_bg_b = location_bgs[cur_location];
+
+                alphaA = 255;
+                alphaB = 0;
+
+                location_bg_a_destAlpha = 0;
+                location_bg_b_destAlpha = 255;
+            }
+            else if(event.key.code == sf::Keyboard::Left)
+            {
+                //mapXdest += float(123);
+
+                //if(mapXdest >= 0)
+                //mapXdest = 0;
+
+                sel_location--;
+
+                if(sel_location <= 0)
+                sel_location = 0;
+            }
+            else if(event.key.code == sf::Keyboard::Right)
+            {
+                //mapXdest -= float(123);
+
+                //int maxBound = (worldmap_fields.size()*123 - 1012) * (-1);
+
+                //if(mapXdest <= maxBound)
+                //mapXdest = maxBound;
+
+                sel_location++;
+
+                if(sel_location >= worldmap_fields.size()-1)
+                sel_location = worldmap_fields.size()-1;
+            }
         }
+        else if (event.type == sf::Event::MouseButtonReleased)
+        {
 
+        }
     }
 }
 
 void ObeliskMenu::Update(sf::RenderWindow &window, float fps)
 {
-    if(isActive){
+    if(isActive)
+    {
+        render_map.clear(sf::Color::Transparent);
 
-        mm_inventory_background.setPosition(0,0);
-        window.draw(mm_inventory_background);
+        if(round(mapX) != mapXdest)
+        {
+            float speed = abs(mapX - mapXdest) * 10;
 
-        sf::Vector2i pixelPos = sf::Mouse::getPosition(window);
-        auto lastView = window.getView();
-        sf::Vector2f worldPos = window.mapPixelToCoords(pixelPos,lastView);
+            if(mapX > mapXdest)
+            mapX -= speed / fps;
+            else
+            mapX += speed / fps;
+        }
 
-        //t_pressToContinue.setPosition(window.getSize().x/2,300);
-        //window.draw(t_pressToContinue);
+        if(mapX >= 0)
+        mapX = 0;
 
-        obeliskButtonList.Update(window, fps, &worldPos);
+        int maxBound = (worldmap_fields.size()*123 - 1012) * (-1);
 
+        if(mapX <= maxBound)
+        mapX = maxBound;
+
+        for(int i=0; i<worldmap_fields.size(); i++)
+        {
+            worldmap_fields[i].setPosition(i*123 + mapX, 0);
+            worldmap_fields[i].update(window);
+
+            render_map.draw(worldmap_fields[i].s);
+        }
+
+        location_highlight.setOrigin(location_highlight.getLocalBounds().width/2, location_highlight.getLocalBounds().height/2);
+        location_highlight.setPosition(sel_location*123 + mapX - 63, 70);
+        location_highlight.update(window);
+        render_map.draw(location_highlight.s);
+
+        for(int i=0; i<worldmap_fields.size(); i++)
+        {
+            worldmap_icons[i].setOrigin(worldmap_icons[i].getLocalBounds().width/2, worldmap_icons[i].getLocalBounds().height);
+            worldmap_icons[i].setPosition(i*123 + mapX + 61, 155);
+            worldmap_icons[i].update(window);
+
+            render_map.draw(worldmap_icons[i].s);
+
+            if(std::find(unlocked.begin(), unlocked.end(), i) == unlocked.end())
+            {
+                unavailable.setPosition(i*123 + mapX - 1, 0);
+                unavailable.update(window);
+                render_map.draw(unavailable.s);
+            }
+        }
+
+        render_map.display();
+
+        if(!displayMissions)
+        {
+            mainbox_destX = 640;
+            mainbox_destY = 320;
+        }
+        else
+        {
+            mainbox_destX = 640;
+            mainbox_destY = 230;
+        }
+
+        if(round(mainbox.lx) != mainbox_destX)
+        {
+            float speed = abs(mainbox.lx - mainbox_destX) * 10;
+
+            if(mainbox.lx > mainbox_destX)
+            mainbox.lx -= speed / fps;
+            else
+            mainbox.lx += speed / fps;
+        }
+
+        if(round(mainbox.ly) != mainbox_destY)
+        {
+            float speed = abs(mainbox.ly - mainbox_destY) * 10;
+
+            if(mainbox.ly > mainbox_destY)
+            mainbox.ly -= speed / fps;
+            else
+            mainbox.ly += speed / fps;
+        }
+
+        if(round(alphaA) != location_bg_a_destAlpha)
+        {
+            float speed = abs(mainbox.ly - mainbox_destY) * 800;
+
+            if(alphaA > location_bg_a_destAlpha)
+            alphaA -= speed / fps;
+            else
+            alphaA += speed / fps;
+        }
+
+        alphaB = 255 - alphaA;
+
+        location_bg_a.setColor(sf::Color(255,255,255,255));
+        location_bg_b.setColor(sf::Color(255,255,255,alphaB));
+
+        location_bg_a.draw(window);
+        location_bg_b.draw(window);
+
+        mainbox.draw(window);
+
+        worldmap_title.setOrigin(worldmap_title.getLocalBounds().width/2, worldmap_title.getLocalBounds().height/2);
+        worldmap_title.setPosition(mainbox.getPosition().x-303, mainbox.getPosition().y-174);
+        worldmap_title.draw(window);
+
+        location_title.setOrigin(location_title.getLocalBounds().width/2, location_title.getLocalBounds().height/2);
+        location_title.setPosition(mainbox.getPosition().x, mainbox.getPosition().y+72);
+        location_title.draw(window);
+
+        location_desc.setOrigin(location_desc.getLocalBounds().width/2, location_desc.getLocalBounds().height/2);
+        location_desc.setPosition(mainbox.getPosition().x, mainbox.getPosition().y+142);
+        location_desc.draw(window);
+
+        float resRatioX = window.getSize().x / float(1280);
+        float resRatioY = window.getSize().y / float(720);
+
+        tex_render_map = render_map.getTexture();
+        spr_render_map.setTexture(tex_render_map, true);
+        spr_render_map.setPosition(sf::Vector2f(134*resRatioX, (mainbox.getPosition().y - 115) * resRatioY));
+        window.draw(spr_render_map);
+
+        if(displayMissions)
+        {
+            missionbox.draw(window);
+            descbox.draw(window);
+
+            select_quest.setOrigin(0, select_quest.getLocalBounds().height/2);
+            select_quest.setPosition(missionbox.getPosition().x - 141, missionbox.getPosition().y - 74);
+            select_quest.draw(window);
+
+            mission_title.setOrigin(0, mission_title.getLocalBounds().height/2);
+            mission_title.setPosition(descbox.getPosition().x - 315, descbox.getPosition().y - 74);
+            mission_title.draw(window);
+
+            mission_desc.setOrigin(0, mission_desc.getLocalBounds().height/2);
+            mission_desc.setPosition(descbox.getPosition().x - 315, descbox.getPosition().y - 41);
+            mission_desc.draw(window);
+        }
     }
 }
 
-void ObeliskMenu::UpdateButtons(){
+void ObeliskMenu::UpdateButtons()
+{
 
 }
-void ObeliskMenu::OnExit(){
+
+void ObeliskMenu::OnExit()
+{
 
 }
+
 ObeliskMenu::~ObeliskMenu()
 {
     //dtor
--- Engine/Menu/Obelisk.h
+++ Engine/Menu/Obelisk.h
@@ -3,22 +3,60 @@
 #include <SFML/Graphics.hpp>
 #include "../Config.h"
 #include "../Graphics/Menu.h"
+#include "../Graphics/PText.h"
 #include "ButtonLists/ObeliskButtonList.h"
 class V4Core;
 class PatapolisMenu;
 class ObeliskMenu : public Menu
 {
     public:
+        sf::Font font;
 
-        sf::Font f_font;
-        sf::Text t_titlemenu;
-        sf::Text t_itemtitle;
+        ///worldmap contents
+        vector<PSprite> location_bgs;
+        vector<PSprite> location_icons;
+        vector<PSprite> mission_icons;
+        vector<PSprite> worldmap_icons;
+        vector<PSprite> worldmap_fields;
+        PSprite location_bg_a, location_bg_b;
+        PSprite dullpon;
 
-        sf::RectangleShape mm_inventory_background;
+        ///boxes
+        PSprite mainbox,descbox,iconbox,missionbox;
+        PSprite missionselect;
+
+        ///text
+        PText worldmap_title;
+        PText location_title;
+        PText location_desc;
+        PText select_quest;
+        PText mission_title;
+        PText mission_desc;
+
+        int cur_location = 0;
+        float mainbox_destX=640,mainbox_destY=360;
+        float alphaA = 0;
+        float alphaB = 255;
+        float location_bg_a_destAlpha = 0;
+        float location_bg_b_destAlpha = 255;
+
+        bool displayMissions = false;
+
+        PSprite unavailable;
+        PSprite location_highlight;
+
+        int sel_location = 0;
+
+        vector<int> unlocked = {0, 1, 2};
+
+        sf::RenderTexture render_map;
+        sf::Texture tex_render_map;
+        sf::Sprite spr_render_map;
+
+        float mapX = 0;
+        float mapXdest = 0;
 
         PatapolisMenu *parentMenu;
-        Camera camera;
-        ObeliskButtonList obeliskButtonList;
 
         void Initialise(Config *thisConfig, std::map<int,bool> *keymap,V4Core *parent,PatapolisMenu *curParentMenu);
         void Update(sf::RenderWindow &window, float fps);
