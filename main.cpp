163913faf74ace8c2cce11633eb31ce4296b34c3
owocek <owocektv@gmail.com>
2020-09-19 13:49:11 +0200

    Display current SFML version

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ string ExePath() {
 }
 int main(int argc, char *argv[])
 {
+    std::cout << SFML_VERSION_MAJOR << "." << SFML_VERSION_MINOR << "." << SFML_VERSION_PATCH << std::endl;
+
     cout << "V4" << endl;
     cout << "my directory is " << ExePath() << "\n";
     //_setmode(_fileno(stdout), _O_U16TEXT);
