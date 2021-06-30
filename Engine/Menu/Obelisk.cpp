ca54e3fd8809dc0e90bbbc1ad1823a53883702df
ArtainR <52716201+ArtainR@users.noreply.github.com>
2021-07-01 01:59:04 +0200

    Formatting + more readability
    As per review

--- Engine/Menu/Obelisk.cpp
+++ Engine/Menu/Obelisk.cpp
@@ -153,26 +153,25 @@ void ObeliskMenu::Reload()
     fld.loadFromFile("resources/graphics/ui/worldmap/location_field.png", quality, 1);
 
     ///worldmap contents
-	// i is location id, not an idx in fields_unlocked
+    // i is location id, not an idx in fields_unlocked
     for (int i = 1; i <= *std::max_element(fields_unlocked.begin(), fields_unlocked.end()); i++)
     {
-		if(std::find(fields_unlocked.begin(), fields_unlocked.end(), i) != fields_unlocked.end())
-		{
-			int curIdx = std::find(fields_unlocked.begin(), fields_unlocked.end(), i) - fields_unlocked.begin();
+        if (std::find(fields_unlocked.begin(), fields_unlocked.end(), i) != fields_unlocked.end())
+        {
+            int curIdx = std::find(fields_unlocked.begin(), fields_unlocked.end(), i) - fields_unlocked.begin();
 
-			PSprite bg;
-			location_bgs.push_back(bg);
-			location_bgs[location_bgs.size() - 1].loadFromFile("resources/graphics/ui/worldmap/locationbg_" + to_string(fields_unlocked[curIdx]) + ".png", quality, 1);
+            PSprite bg;
+            location_bgs.push_back(bg);
+            location_bgs[location_bgs.size() - 1].loadFromFile("resources/graphics/ui/worldmap/locationbg_" + to_string(fields_unlocked[curIdx]) + ".png", quality, 1);
 
-			PSprite loc;
-			loc.loadFromFile("resources/graphics/ui/worldmap/location_" + to_string(fields_unlocked[curIdx]) + ".png", quality, 1);
-			worldmap_icons.push_back(loc);
-		}
-		else
-		{
-			worldmap_fields.push_back(fld);
-			worldmap_icons.push_back(dullpon);
-		}
+            PSprite loc;
+            loc.loadFromFile("resources/graphics/ui/worldmap/location_" + to_string(fields_unlocked[curIdx]) + ".png", quality, 1);
+            worldmap_icons.push_back(loc);
+        } else
+        {
+            worldmap_fields.push_back(fld);
+            worldmap_icons.push_back(dullpon);
+        }
     }
 
     for (int i = 1; i <= 20; i++)
@@ -188,18 +187,18 @@ void ObeliskMenu::Reload()
         }
     }
 
-	if(sel_location > 0 && sel_location < location_bgs.size())
-	{
-		location_bg_a = location_bgs[sel_location - 1];
-		location_bg_b = location_bgs[sel_location - 1];
-	}
+    if (sel_location > 0 && sel_location < location_bgs.size())
+    {
+        location_bg_a = location_bgs[sel_location - 1];
+        location_bg_b = location_bgs[sel_location - 1];
+    }
 
     unlocked.clear();
 
-    for (int i = 0; i < fields_unlocked.size(); i++)
-	{
-        unlocked.push_back(fields_unlocked[i] - 1);
-	}
+    for (const auto& field : fields_unlocked)
+    {
+        unlocked.push_back(field - 1);
+    }
 
     //vector<int> unlocked = {0};
 }
