f4552a4d690304764d751bd93eb97e7e40d04b0b
owocek <owocektv@gmail.com>
2022-02-19 00:05:29 +0100

    config argument no longer necessary

--- Engine/Graphics/ScreenFade.cpp
+++ Engine/Graphics/ScreenFade.cpp
@@ -1,14 +1,46 @@
 #include "ScreenFade.h"
 #include <math.h>
 
+#include "../CoreManager.h"
+
 ScreenFade::ScreenFade()
 {
 }
 
+
+//TO-DO: fully replace this create function with one that doesnt have config in arguments
 void ScreenFade::Create(Config* config, int mode, float speed)
 {
     cur_mode = mode;
     cur_speed = speed;
+
+    r_rect.setSize(sf::Vector2f(config->GetInt("resX"), config->GetInt("resY")));
+    //r_rect.setFillColor(sf::Color::Black);
+
+    switch (cur_mode)
+    {
+        case 0: {
+            alpha = 255;
+            dest_alpha = 0;
+            r_rect.setFillColor(sf::Color(0, 0, 0, alpha));
+            break;
+        }
+
+        case 1: {
+            alpha = 0;
+            dest_alpha = 255;
+            r_rect.setFillColor(sf::Color(0, 0, 0, alpha));
+            break;
+        }
+    }
+}
+
+void ScreenFade::Create(int mode, float speed)
+{
+    cur_mode = mode;
+    cur_speed = speed;
+
+    Config* config = CoreManager::getInstance().getConfig();
 
     r_rect.setSize(sf::Vector2f(config->GetInt("resX"), config->GetInt("resY")));
     //r_rect.setFillColor(sf::Color::Black);
@@ -47,6 +79,7 @@ bool ScreenFade::checkFinished()
         return false;
 }
 
+//TO-DO: fully replace this draw function with one that doesnt have window and fps in arguments
 void ScreenFade::draw(sf::RenderWindow& window, float fps)
 {
     if (dest_alpha < alpha)
@@ -65,4 +98,27 @@ void ScreenFade::draw(sf::RenderWindow& window, float fps)
 
     r_rect.setFillColor(sf::Color(0, 0, 0, alpha));
     window.draw(r_rect);
+}
+
+void ScreenFade::draw()
+{
+    float fps = CoreManager::getInstance().getCore()->getFPS();
+    sf::RenderWindow* window = CoreManager::getInstance().getWindow();
+
+    if (dest_alpha < alpha)
+    {
+        alpha -= cur_speed / fps;
+    } else if (dest_alpha > alpha)
+    {
+        alpha += cur_speed / fps;
+    }
+
+    if (alpha <= 0)
+        alpha = 0;
+
+    if (alpha >= 255)
+        alpha = 255;
+
+    r_rect.setFillColor(sf::Color(0, 0, 0, alpha));
+    window->draw(r_rect);
 }
