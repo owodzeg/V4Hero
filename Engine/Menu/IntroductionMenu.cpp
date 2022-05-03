78f3ccc8e3a433fecedcefa23ceb2b22e50c6982
owocek <owocektv@gmail.com>
2022-05-03 20:54:46 +0200

    Revert "restart introduction menu timeout"
    This reverts commit bbadd09bc223eae3b0c3eb1a9479cedab6168462.

--- Engine/Menu/IntroductionMenu.cpp
+++ Engine/Menu/IntroductionMenu.cpp
@@ -29,6 +29,8 @@ void IntroductionMenu::Initialize()
     msgcloud.AddDialog(Func::ConvertToUtf8String(CoreManager::getInstance().getStrRepo()->GetString("newgame_intro_13")), true);
     msgcloud.AddDialog(Func::ConvertToUtf8String(CoreManager::getInstance().getStrRepo()->GetString("newgame_intro_14")), true);
 
+    timeout.restart(); 
+
     SPDLOG_DEBUG("Introduction menu initialized.");
 }
 
@@ -55,8 +57,6 @@ void IntroductionMenu::Update()
 
     if (timeout2.getElapsedTime().asSeconds() > 3)
     {
-        StateManager::getInstance().setState(StateManager::MISSIONCONTROLLER);
-
         //TO-DO: change state in statemanager
 
         /*
