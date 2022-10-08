f50cec67cd5892ce9da5cb0823e4cbd66f37b05c
owocek <owocektv@gmail.com>
2022-10-08 09:45:26 +0200

    work in progress new animation implementation

--- Engine/Mission/Units/PNGAnimation.cpp
+++ Engine/Mission/Units/PNGAnimation.cpp
@@ -1,8 +1,50 @@
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+
 #include "PNGAnimation.h"
+#include <filesystem>
+#include <spdlog/spdlog.h>
+
+namespace fs = std::filesystem;
 
 PNGAnimation::PNGAnimation()
 {
 
+}
+
+void PNGAnimation::Load(const std::string& path)
+{
+    SPDLOG_INFO("Loading PNGAnimation model from {}", path);
+
+    auto f = fs::path(path);
+    
+    if (f.extension() == ".zip")
+    {
+        SPDLOG_INFO(".zip was provided, we need to extract the file");
+    } else if (fs::is_directory(path))
+    {
+        SPDLOG_INFO("directory was provided, no need to extract the file");
+
+        //Step 1: Load animation names and frame paths
+        for (const auto& entry : fs::directory_iterator(path))
+        {
+            Animation tmp;
+
+            SPDLOG_DEBUG("Animation found: {}", entry.path().string());
+            tmp.name = entry.path().string();
+
+            for (const auto& frame : fs::directory_iterator(entry.path()))
+            {
+                SPDLOG_DEBUG("Animation frame found: {}", frame.path().string());
+                tmp.frame_paths.push_back(frame.path().string());
+            }
+
+            animations.push_back(tmp);
+        }
+
+        //Step 2: Compose frames into a spritesheet for each animation
+
+        //Step 3: Load spritesheets to ResourceManager
+    }
 }
 
 void PNGAnimation::Draw()
--- Engine/Mission/Units/PNGAnimation.h
+++ Engine/Mission/Units/PNGAnimation.h
@@ -2,11 +2,21 @@
 #define PNGANIMATION_H
 
 #include <SFML/Graphics.hpp>
+#include <vector>
 
 class PNGAnimation
 {
     public:
+        struct Animation {
+            std::string name;
+            std::vector<std::string> frame_paths;
+            std::vector<sf::Image> spritesheets;
+        };
+
+        std::vector<Animation> animations;
+
         PNGAnimation();
+        virtual void Load(const std::string& path);
         virtual void Draw();
 };
 
