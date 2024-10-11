f0d91b4ac2f536228b343e03c985113a3df2aeb3
owocek <owocektv@gmail.com>
2024-10-11 14:16:00 +0200

    fever worm improvements

--- Engine/Mission/Units/Unit/FeverWorm.cpp
+++ Engine/Mission/Units/Unit/FeverWorm.cpp
@@ -8,7 +8,9 @@ FeverWorm::FeverWorm()
     main.setAnimation("slow");
 
     global_x = 300;
-    global_y = 850;
+    global_y = 720;
+
+    local_x = -1600;
 }
 
 void FeverWorm::PrefeverSpeedUp()
@@ -26,9 +28,11 @@ void FeverWorm::Transform()
     if(!fever)
     {
         main.setAnimation("transform");
+        main.restartAnimation();
         fever = true;
         t_done = false;
         t_timer.restart();
+        local_x = 0;
     }
 }
 
@@ -36,6 +40,7 @@ void FeverWorm::Break()
 {
     main.setAnimation("slow");
     fever = false;
+    local_x = -1600;
 }
 
 void FeverWorm::Draw()
@@ -47,23 +52,48 @@ void FeverWorm::Draw()
 
     float fps = CoreManager::getInstance().getCore()->getFPS();
 
+    auto r = CoreManager::getInstance().getRhythm();
+
+    float destPrefeverOffset = (r->satisfaction / r->getAccRequirement(r->combo));
+
+    if(r->combo >= 2 && !fever)
+    {
+        if(r->advanced_prefever)
+            main.setAnimation("fast");
+        else
+            main.setAnimation("slow");
+
+        local_x_dest = -500 + (destPrefeverOffset * 500);
+
+        float delta = 1/fps * (4 * (main.animation.animationSpeed / 60.6));
+        Func::smoothTransition(local_x, local_x_dest, delta);
+    }
+
     if(fever && !t_done)
     {
-        if(t_timer.getElapsedTime().asMilliseconds() >= 21*(1 / main.animation.animationSpeed) && t_timer.getElapsedTime().asMilliseconds() < 120*(1 / main.animation.animationSpeed))
+        if(t_timer.getElapsedTime().asMilliseconds() >= 24*(1000 / main.animation.animationSpeed) && t_timer.getElapsedTime().asMilliseconds() < 35*(1000 / main.animation.animationSpeed))
         {
-            local_x -= 300.f / fps;
-        }
-
-
-        if(t_timer.getElapsedTime().asMilliseconds() >= 120*(1 / main.animation.animationSpeed))
-        {
-            if(local_x < 120)
+            if(local_x > -1600)
             {
-                local_x += 500.f / fps;
+                local_x -= 6500.f * (main.animation.animationSpeed/60.6) / fps;
+            } else
+            {
+                local_x = -1600;
             }
         }
 
-        if(t_timer.getElapsedTime().asMilliseconds() >= 150*(1 / main.animation.animationSpeed))
+
+        if(t_timer.getElapsedTime().asMilliseconds() >= 50*(1000 / main.animation.animationSpeed))
+        {
+            main.setAnimation("fever");
+
+            if(local_x < 120)
+            {
+                local_x += 1600.f * (main.animation.animationSpeed/60.6) / fps;
+            }
+        }
+
+        if(t_timer.getElapsedTime().asMilliseconds() >= 80*(1000 / main.animation.animationSpeed))
         {
             if(local_x >= 120)
             t_done = true;
--- Engine/Mission/Units/Unit/FeverWorm.h
+++ Engine/Mission/Units/Unit/FeverWorm.h
@@ -7,7 +7,7 @@ class FeverWorm
 {
 public:
     AnimatedObject main;
-    float local_x=0, local_y=0;
+    float local_x=0, local_y=0, local_x_dest=0;
     float global_x=0, global_y=0;
 
     bool fever = false;
