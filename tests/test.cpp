8cdda1d2dd1fbf1e386961e98cb80b5350a3434d
Giovanni Berti <dev.giovanniberti@gmail.com>
2024-11-17 12:28:53 +0100

    added test case for serialization

--- tests/test.cpp
+++ tests/test.cpp
@@ -7,7 +7,7 @@
 
 using json = nlohmann::json;
 
-TEST_CASE("deserialization", "is ok")
+TEST_CASE("save file deserialization", "is ok")
 {
     std::ifstream conf("test-resources/new_save.p4sv", std::ios::in);
     json save_data;
@@ -134,4 +134,133 @@ TEST_CASE("deserialization", "is ok")
     };
 
     REQUIRE(save_file == expected_save_file);
+}
+
+TEST_CASE("save file serialization", "is ok")
+{
+    std::ifstream conf("test-resources/new_save.p4sv", std::ios::in);
+    json expected_serialized_data;
+    conf >> expected_serialized_data;
+
+    SaveFile save_file = SaveFile{
+        .save = SaveData{
+            .army = Army{
+                .hero = SquadSlot{
+                    .rarepon = -1,
+                    ._class = 0,
+                    .level = 0,
+                    .exp = 0,
+                    .slots = {},
+                },
+                .squads = {
+                    {
+                        SquadSlot{
+                            .rarepon = 0,
+                            ._class = 0,
+                            .level = 1,
+                            .exp = 0,
+                            .slots = {
+                                {3, 0, 0},
+                                {4, 1, 0},
+                                {-1},
+                                {-1},
+                                {-1}
+                            }
+                        },
+                        SquadSlot{
+                            .rarepon = 1,
+                            ._class = 0,
+                            .level = 1,
+                            .exp = 0,
+                            .slots = {
+                                {3, 0, 0},
+                                {4, 1, 0},
+                                {-1},
+                                {-1},
+                                {-1}
+                            }
+                        },
+                        SquadSlot{
+                            .rarepon = 2,
+                            ._class = 0,
+                            .level = 1,
+                            .exp = 0,
+                            .slots = {
+                                {3, 0, 0},
+                                {4, 1, 0},
+                                {-1},
+                                {-1},
+                                {-1}
+                            }
+                        },
+                        SquadSlot{
+                            .rarepon = 3,
+                            ._class = 0,
+                            .level = 1,
+                            .exp = 0,
+                            .slots = {
+                                {3, 0, 0},
+                                {4, 1, 0},
+                                {-1},
+                                {-1},
+                                {-1}
+                            }
+                        },
+                        SquadSlot{
+                            .rarepon = 4,
+                            ._class = 0,
+                            .level = 1,
+                            .exp = 0,
+                            .slots = {
+                                {3, 0, 0},
+                                {4, 1, 0},
+                                {-1},
+                                {-1},
+                                {-1}
+                            }
+                        },
+                        SquadSlot{
+                            .rarepon = 5,
+                            ._class = 0,
+                            .level = 1,
+                            .exp = 0,
+                            .slots = {
+                                {3, 0, 0},
+                                {4, 1, 0},
+                                {-1},
+                                {-1},
+                                {-1}
+                            }
+                        }
+                    }
+                }
+            },
+            .details = SaveFileDetails{
+                .version = "2.0",
+                .name = "Kamipon",
+                .locations_unlocked = {1},
+                .story_point = 1
+            },
+            .items = {
+                SavedItem{
+                    .name = "item_wooden_spear",
+                    .count = 6
+                },
+                SavedItem{
+                    .name = "item_wooden_helmet",
+                    .count = 6
+                }
+            },
+            .missions = {
+                SavedMission{
+                    .id = 1,
+                    .level = 0,
+                    .unlocked = true
+                }
+            }
+        }
+    };
+
+    json serialized_data = save_file;
+    REQUIRE(serialized_data == expected_serialized_data);
 }
